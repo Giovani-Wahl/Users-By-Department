50e06b4790d1faf2e34cfcf4dc7081033a7424f6
wahl <giovaniwahl@gmail.com>
2023-10-24 11:58:33 -0300

    inicio do projeto

---
+++ src/main/java/com/giovani/usersbydepartment/UsersByDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.giovani.usersbydepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersByDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersByDepartmentApplication.class, args);
+	}
+
+}
